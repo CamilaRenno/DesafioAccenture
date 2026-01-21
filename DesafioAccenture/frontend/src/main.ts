3ebaab4a458a299626ac3493c9500634739c6675
CamilaRenno <34922418+CamilaRenno@users.noreply.github.com>
2026-01-21 11:15:49 -0300

    Add files via upload

---
+++ DesafioAccenture/frontend/src/main.ts
@@ -0,0 +1,5 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+
+bootstrapApplication(AppModule as any);
